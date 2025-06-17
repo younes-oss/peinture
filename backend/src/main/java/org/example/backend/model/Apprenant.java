1e372a9c8f3560dffc289ad291eece5589974365
younes-oss <aitchattouyounes@gmail.com>
2025-06-17 13:30:22 +0200

    terminer la structure

--- backend/src/main/java/org/example/backend/model/Apprenant.java
+++
@@ -1,9 +0,0 @@
-package org.example.backend.model;
-
-public class Apprenant {
-    private int id;
-    private String name;
-    private String address;
-    private String phone;
-
-}
---
+++ backend/src/main/java/org/example/backend/model/Panier.java
@@ -0,0 +1,5 @@
+package org.example.backend.model;
+
+public class Panier {
+
+}
